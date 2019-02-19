3615fffb15711c1188a80765d3416a99db7e6e42
alumno <alumno@2A4.labs.academico>
2019-02-19 11:00:24 -0600

    Mejoramiento de Tercera Version(documentacion)

--- src/control_planilla/Control_Planilla.java
+++ src/control_planilla/Control_Planilla.java
@@ -11,26 +11,24 @@ public class Control_Planilla {
     static Llenado_Planilla lp = new Llenado_Planilla();
 
     public static void main(String[] args) {
-        int iOpcion;
-        int iFila = 10;
-        int iColumna = 10;
-        int iDeptos[] = new int[5];
-        String[][] sPlanilla = new String[iFila][iColumna];
-        do {
+        int iOpcion;//Variable de Opcion de Menú
+        int iDeptos[] = new int[5];//Vector de Departamentos
+        String[][] sPlanilla = new String[10][10];//Matriz donde se guardará la planilla
+        do {//Ciclo que se genera si la opcion no sea igual a 0
             iOpcion = Integer.parseInt(JOptionPane.showInputDialog(null, "1.Ingreso de Datos\n0.Salir", "MENÚ", JOptionPane.PLAIN_MESSAGE));
             menu(iOpcion, sPlanilla, iDeptos);
         } while (iOpcion != 0);
-        System.exit(0);
+        System.exit(0);//Termina en programa
     }
 
     public static void menu(int iOpcion, String[][] sPlanilla, int[] iDeptos) {
         switch (iOpcion) {
             case 1:
-                lp.Llenado_General(sPlanilla, iDeptos);
+                lp.Llenado_General(sPlanilla, iDeptos);//Llama el procedimiento de la clase Llenado Planilla
                 System.out.print("CÓDIGO\t\t" + "NOMBRE\t\t" + "SUELDO BASE\t" + "BONO\t\t" + "COMISION\t" + "IGSS\t" + "DESCUENTO "
                         + "JUDICIAL\t" + "ISR\t" + "SUELDO LIQUIDO\t\t" + "DEPTO." + "\n");
 
-                for (int i = 0; i < sPlanilla.length; i++) {
+                for (int i = 0; i < sPlanilla.length; i++) {//Imprime la matriz de la planilla
                     for (int j = 0; j < sPlanilla[i].length; j++) {
                         System.out.print(sPlanilla[i][j] + "\t" + "\t");
                     }
@@ -38,7 +36,7 @@ public class Control_Planilla {
                 }
                 System.out.print("TOTAL POR DEPARTAMENTOS\n");
                 System.out.print("1\t\t"+"2\t\t"+"3\t\t"+"4\t\t"+"5\t\t\n");
-                for (int j = 0; j < 5; j++) {
+                for (int j = 0; j < 5; j++) {//Imprime el vector de departamentos
                     System.out.print(iDeptos[j] + "\t" + "\t");
                 }
                 System.out.println();
